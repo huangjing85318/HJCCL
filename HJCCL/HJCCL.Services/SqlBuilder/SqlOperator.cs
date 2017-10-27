13a30f3355453a356499a9fe4be9ae7b4897bdbb
huangjing <99915945@qq.com>
2017-10-27 10:36:30 +0800

    添加sql语句操作类、枚举

---
+++ HJCCL/HJCCL.Services/SqlBuilder/SqlOperator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HJCCL.Services.SqlBuilder
+{
+    public enum SqlOperator
+    {
+        /// <summary>
+        /// 范围
+        /// </summary>
+        [Description("范围")]
+        Between,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Description("等于")]
+        Equal,
+
+
+        /// <summary>
+        /// 等于
+        /// </summary>
+        [Description("不等于")]
+        NotEqual,
+
+        /// <summary>
+        /// 不等于
+        /// </summary>
+        [Description("大于等于")]
+        GreaterThanEqual,
+
+        /// <summary>
+        /// 包含
+        /// </summary>
+        [Description("包含")]
+        In,
+
+        /// <summary>
+        /// 小于
+        /// </summary>
+        [Description("小于")]
+        LessThan,
+
+        /// <summary>
+        /// 小于等于
+        /// </summary>
+        [Description("小于等于")]
+        LessThanEqual,
+
+        /// <summary>
+        /// 模糊查找
+        /// </summary>
+        [Description("模糊查找")]
+        Like,
+
+        /// <summary>
+        /// 左边模糊查找
+        /// </summary>
+        [Description("左边模糊查找")]
+        LikeLeft,
+
+        /// <summary>
+        /// 右边模糊查找
+        /// </summary>
+        [Description("右边模糊查找")]
+        LikeRight,
+
+    }
+}
---
+++ HJCCL/HJCCL.Services/SqlBuilder/WhereCondition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HJCCL.Services.SqlBuilder
+{
+    public class WhereCondition
+    {
+        public void AddCondition(string field, object value, SqlOperator sqlOperator, bool condition = true)
+        {
+            if (field.Length > 0)
+            {
+                if (value != null)
+                {
+                    if (sqlOperator == SqlOperator.Between)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
